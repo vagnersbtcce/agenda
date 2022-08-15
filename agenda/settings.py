8f1c3b7be6353accb307a63715f6c9cfad732dc4
Vagner Begni - TCCE <107005740+vagnersbtcce@users.noreply.github.com>
2022-08-14 23:34:17 -0300

    Update settings.py

--- agenda/settings.py
+++ agenda/settings.py
@@ -80,9 +80,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'agenda',
-        'USER': 'tccedev',
+        'USER': '************',
         'HOST': 'localhost',
-        'PASSWORD': 'Postgr3SQL@1311',
+        'PASSWORD': '************',
         'PORT': '5432',
     }
 }
